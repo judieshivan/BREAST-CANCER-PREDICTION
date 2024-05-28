9e3d16a048c8540e06713c44b7fd99f4bdef5b3b
Shenzlule <shenzlule65@gmail.com>
2024-05-28 20:40:21 +0300

    commit working 2.0

--- app.py
+++ app.py
@@ -1,35 +1,56 @@
 import os
 import pathlib
 
-import requests
-from flask import Flask, session, abort, redirect, request, render_template
-import math
-import pickle
+import joblib
 import numpy as np
-from google.oauth2 import id_token
-from google_auth_oauthlib.flow import Flow
-from pip._vendor import cachecontrol
-import google.auth.transport.requests
+from flask import Flask, session, abort, redirect, request, render_template
 
-#
+
 app = Flask("Breast Cancer Prediction")
 
-
-
+scaler = joblib.load('./models/scaler.pkl')  # The scaler fitted on the training data
+pca = joblib.load('./models/pca.pkl')  # The PCA fitted on the training data
+logreg = joblib.load('./models/logreg.pkl')  # The trained RandomForestClassifier
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
 
-@app.route("/about")
-def about():
-    return render_template("about.html")
+@app.route("/pred1")
+def pred1():
+    return render_template("pred1.html")
 
 
 @app.route("/contact")
 def contact():
     return render_template("contact.html")
+
+@app.route("/pred1_", methods=['POST', 'GET'])
+def pred1_():
+    int_features = [x for x in request.form.values()]
+    if not int_features or "" in int_features:
+        return render_template("pred1.html", prediction="Please enter all required features.")
+
+    int_features = [float(x) for x in int_features]
+    print(int_features)
+    array_numpy_features = np.array(int_features).reshape(1, -1)
+
+    # Preprocess the input features
+    X_scaled = scaler.transform(array_numpy_features)  # Scale the input features
+    X_pca = pca.transform(X_scaled)  # Apply PCA transformation
+
+    # Predict using the trained classifier
+    y_pred = logreg.predict(X_pca)
+    print("Predicted values for new user input data:", y_pred)
+
+    return render_template("pred1.html", prediction=f'Your predicted value is : {y_pred[0]}')
+
+    return render_template("pred1.html")
+
+
+
+
 
 
 
